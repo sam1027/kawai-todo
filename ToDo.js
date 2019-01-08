d78521b3a0575778da15104e53dfddf67c5600b5
sam1027 <yse1027@gmail.com>
2019-01-09 00:17:14 +0900

    coding the UI part2

---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+
+export default class ToDo extends Component{
+    render(){
+        return(
+            <View>
+                <Text>hello i'm todo!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+}) 
